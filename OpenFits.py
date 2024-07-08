a698aac815f1214ed80aa1072ae754bb30ab857b
Nora Malgeri <noramalgeri@macnora.dyndns.cern.ch>
2024-07-08 11:33:34 +0200

    adding library to open fits files, and an initial image

---
+++ OpenFits.py
@@ -0,0 +1,15 @@
+from astropy.io import fits
+import matplotlib.pyplot as plt
+import numpy as np
+
+def OpenFits(file, BoW=0):
+    f = fits.open(file)
+    d = f[0].data                    # 2-D array with image
+    vminVal = 0.95*np.median(d)      # may need to adjust
+    vmaxVal = 1.1*np.median(d)
+    cmapVal = 'gray'                 # white stars on black background
+    if BoW == 0:
+        cmapVal = 'Greys'            # black stars on white background
+    plt.imshow(d, vmin=vminVal, vmax=vmaxVal, cmap=cmapVal, origin='upper')
+    plt.show()
+    return d
