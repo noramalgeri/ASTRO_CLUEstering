f5aca01983dfbd87948fadf3b8bcc87bf4ac97d1
Nora Malgeri <noramalgeri@gmail.com>
2024-07-10 17:12:18 +0200

    added the CLUEstering algorithm to the code. parameters still to be adjusted

--- main.py
+++ main.py
@@ -2,29 +2,30 @@ import OpenFits as of
 import matplotlib.pyplot as plt
 import pandas as pd
 import matplotlib
+import CLUEstering as clue
 
 def percentage(sorted_df):
     # === cut out 98% of data ===
-    threshold = int(len(sorted_df)*0.98)
-    threshold_intensity = sorted_df.iloc[threshold]["Intensity"]
-    sorted_df = sorted_df.drop(df[df["Intensity"]<threshold_intensity].index)
+    threshold = int(len(sorted_df)*0.99)
+    threshold_intensity = sorted_df.iloc[threshold]["weight"]
+    sorted_df = sorted_df.drop(df[df["weight"]<threshold_intensity].index)
     return sorted_df
 
 def derivative(sorted_df):
     # === calculating derivative ===
     gap = 1000
     sorted_df.reset_index(inplace=True, drop=True)
-    derivatives = [0] * len(sorted_df["Intensity"])
+    derivatives = [0] * len(sorted_df["weight"])
 
-    for k in range(len(sorted_df["Intensity"])-gap):
-        dfx = (sorted_df["Intensity"][k+gap] - sorted_df["Intensity"][k])/gap
+    for k in range(len(sorted_df["weight"])-gap):
+        dfx = (sorted_df["weight"][k+gap] - sorted_df["weight"][k])/gap
         derivatives[k]=dfx
 
     sorted_df = sorted_df.assign(derivative = derivatives)
     # print(sorted_df.head)
 
     # === plot of derivative ===
-    plt.scatter(range(len(sorted_df["Intensity"])), derivatives, s=0.01)
+    plt.scatter(range(len(sorted_df["weight"])), derivatives, s=0.01)
     plt.show()
 
     # === threshold for derivative ===
@@ -60,12 +61,21 @@ for i in range(len(data)):
 # plt.hist(intensity, log=True, bins=200)
 # plt.show()
 
-df = pd.DataFrame(list(zip(x, y, intensity)), columns=['X', 'Y', 'Intensity'])
+df = pd.DataFrame(list(zip(x, y, intensity)), columns=['x0', 'x1', 'weight'])
 
-sorted_df = df.sort_values(by="Intensity")
+sorted_df = df.sort_values(by="weight")
 
 # === filter data ===
 cutoff = percentage(sorted_df)
+
+dc = 0.0000000000000000000000000000000000000000005 #the side of the box inside of which the density of a point is calculated
+rhoc = 500000 #the minimum energy density that a point must have to not be considered an outlier
+odf = 0.005 # Outlier Delta Factor: multiplied by dc_ gives dm_, the side of the box inside of which the followers of a point are searched
+
+clust = clue.clusterer(dc, rhoc, odf)
+clust.read_data(cutoff)
+clust.run_clue()
+clust.cluster_plotter()
 
 # cutoff = derivative(sorted_df)
 
@@ -75,7 +85,7 @@ cutoff = percentage(sorted_df)
 # norm = matplotlib.colors.Normalize(vmin=min_val, vmax=max_val)
 # color_list = cmap(cutoff["Intensity"])
 
-plt.scatter(cutoff['X'],cutoff["Y"], s=1, c=cutoff["Intensity"], cmap='gist_rainbow', marker='*')
-# plt.scatter(cutoff['X'],cutoff["Y"], s=0.001)
-plt.colorbar()
-plt.show()
+# plt.scatter(cutoff['X'],cutoff["Y"], s=1, c=cutoff["Intensity"], cmap='gist_rainbow', marker='*')
+# # plt.scatter(cutoff['X'],cutoff["Y"], s=0.001)
+# plt.colorbar()
+# plt.show()
